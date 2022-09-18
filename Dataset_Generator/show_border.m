534be51b33be13d3b49331083e01d05de136be5c
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-09-18 20:05:44 +0200

    Create show_border.m

---
+++ Dataset_Generator/show_border.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = show_border(inputArg1,inputArg2)
+%SHOW_BORDER Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
