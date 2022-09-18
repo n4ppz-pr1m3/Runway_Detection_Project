d3595e83fad5266ab072342d47bf03893b9f4039
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-09-18 20:35:42 +0200

    Update generate_poses.m

--- Dataset_Generator/generate_poses.m
+++ Dataset_Generator/generate_poses.m
@@ -1,4 +1,4 @@
-function [poses_data, poses_count] = generate_poses(airports_data, param1, param2, param3, coord_type)
+function [poses_data, poses_count] = generate_poses(airports_data, coord_type, param1, param2, param3)
 
 airports = string(fieldnames(airports_data));
 nAirports = numel(airports);
@@ -9,7 +9,7 @@ for i=1:nAirports
     airport = airports(i);
     airport_data = airports_data.(airport);
     airport_origin = airport_data{1};
-    cameraPoses = coord2poses(param1, param2, param3, coord_type, airport_origin);
+    cameraPoses = coord2poses(coord_type, param1, param2, param3, airport_origin);
     poses_data.(airport) = cameraPoses;
     offset = offset + size(cameraPoses, 1);
 end
