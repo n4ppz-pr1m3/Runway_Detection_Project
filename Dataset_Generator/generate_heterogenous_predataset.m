250649702799d743e7a07c737cbdd53839b3576a
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-09-18 20:38:06 +0200

    Update generate_heterogenous_predataset.m

--- Dataset_Generator/generate_heterogenous_predataset.m
+++ Dataset_Generator/generate_heterogenous_predataset.m
@@ -10,14 +10,14 @@ full_labels_data = cell(nSubDatasets, 1);
 for i=1:nSubDatasets
     disp("Generating sub-dataset " + string(i));
     airports_data = full_airports_data{i};
-    param1 = full_camera_locations{i, 1};
-    param2 = full_camera_locations{i, 2};
-    param3 = full_camera_locations{i, 3};
-    coord_type = full_camera_locations{i, 4};
+    coord_type = full_camera_locations{i, 1};
+    param1 = full_camera_locations{i, 2};
+    param2 = full_camera_locations{i, 3};
+    param3 = full_camera_locations{i, 4};
     calibrationData = full_calibrationData{i};
     check_dir = (i == 1);
     [sub_dataset_size, full_labels_data{i}] = generate_homogenous_predataset(airports_data,...
-                                                            param1, param2, param3, coord_type,...
+                                                            coord_type, param1, param2, param3,...
                                                             calibrationData,...
                                                             preDatasetFolder, pdfImagesFolder,...
                                                             basename, offset, check_dir);
