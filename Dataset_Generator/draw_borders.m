0c43b135fe4adde276d2dc373651d5a87267ebaf
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-09-20 00:49:46 +0200

    Update draw_borders.m

--- Dataset_Generator/draw_borders.m
+++ Dataset_Generator/draw_borders.m
@@ -1,16 +1,17 @@
-function draw_borders(airport, image_path, save_path, fig, runways_corners)
+function draw_borders(airport, image_path, save_path, fig, colors, runways_corners)
 
 imshow(image_path);
 hold on;
 nRunways = size(runways_corners, 3);
 for i=1:nRunways
     runway_corners = runways_corners(:, :, i);
-    plot([runway_corners(1, :), runway_corners(1, 1)], [runway_corners(2, :), runway_corners(2, 1)]);
+    plot([runway_corners(1, :), runway_corners(1, 1)],...
+        [runway_corners(2, :), runway_corners(2, 1)],...
+        "LineWidth", 2, "Color", colors(i, :));
 end
 hold off;
 title(airport)
 saveas(fig, save_path);
-
 
 end
 
