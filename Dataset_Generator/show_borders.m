67e056c6002d330fb5cb7342cc54419adb3eb248
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-09-19 12:49:34 +0200

    Update show_borders.m

--- Dataset_Generator/show_borders.m
+++ Dataset_Generator/show_borders.m
@@ -1,7 +1,9 @@
-function [outputArg1,outputArg2] = show_border(inputArg1,inputArg2)
-%SHOW_BORDER Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function show_border(image_path, corners)
+
+imshow(image_path);
+hold on;
+plot([corners(1, :), corners(1, 1)], [corners(2, :), corners(2, 1)]);
+hold off;
+
 end
 
