5ad65d995af8846be8e9b467d171f7208c1df882
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-09-20 18:05:50 +0200

    Create show_reprojections.m

---
+++ Dataset_Generator/show_reprojections.m
@@ -0,0 +1,45 @@
+function show_reprojections(calibrationData)
+
+close all;
+
+numImages = calibrationData.numImages;
+width = calibrationData.imageSize(2);
+residuals = calibrationData.residuals;
+errors = zeros(numImages, 1);
+for i=1:numImages
+    errors(i) = norm(residuals(i, 1));
+end
+
+imagesPoints = calibrationData.imagesPoints;
+cameraPoints = calibrationData.cameraPoints;
+intrinsicMatrix = calibrationData.intrinsicMatrix;
+reprojectedImagesPoints = cameraPoints * intrinsicMatrix';
+reprojectedImagesPoints = reprojectedImagesPoints(:, 1:2) ./ reprojectedImagesPoints(:, 3);
+
+imagesFolder = calibrationData.imagesFolder;
+reprojectionsFolder = calibrationData.reprojectionsFolder;
+basename = calibrationData.baseFilename;
+ext = calibrationData.imageFormat;
+fig = figure;
+fig.Visible = "off";
+for i=1:numImages
+    filename = fullfile(imagesFolder, basename + "_" + string(i) + "." + ext);
+    reproj_filename = fullfile(reprojectionsFolder, "reproj_" + basename + "_" + string(i) + "." + ext);
+    imshow(filename);
+    hold on;
+    plot(imagesPoints(i, 1), imagesPoints(i, 2), 'r+');
+    plot(reprojectedImagesPoints(i, 1), reprojectedImagesPoints(i, 2), 'go');
+    hold off;
+    if imagesPoints(i, 1) >= width/2
+        lcn = "northwest";
+    else
+        lcn = "northeast";
+    end
+    legend("Original calibration point", "Reprojected calibration point", "Location", lcn);
+    title("Reprojection error : " + string(errors(i)) + " pixels");
+    saveas(fig, reproj_filename);
+end
+close(fig)
+
+end
+
