e3724b660cc55f72f997cb738cdc44baa614d42f
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-09-20 16:54:39 +0200

    Update measure_images_points.m

--- Dataset_Generator/measure_images_points.m
+++ Dataset_Generator/measure_images_points.m
@@ -1,4 +1,4 @@
-function calibrationData = measure_images_points(currentCalibrationData)
+function calibrationData = measure_images_points(currentCalibrationData, varargin)
 
 calibrationData = currentCalibrationData;
 
@@ -7,19 +7,28 @@ baseFilename = calibrationData.baseFilename;
 ext = calibrationData.imageFormat;
 numImages = calibrationData.numImages;
 
-imagePoints = zeros(numImages, 2);
-for i=1:numImages
-    filename = imagesFolder + "/" + baseFilename + "_" + string(i) + "." + ext;
-    %imagePoints(i, :) = mark_image(filename, 1, 1, [], '');
-    imagePoints(i, :) = mark_point(filename, 0.025);
+if nargin == 1
+    % Measure all calibration points images coordinates
+    imagesPoints = zeros(numImages, 2);
+    for i=1:numImages
+        filename = fullfile(imagesFolder, baseFilename + "_" + string(i) + "." + ext);
+        %imagesPoints(i, :) = mark_image(filename, 1, 1, [], '');
+        imagesPoints(i, :) = mark_point(filename, 0.025);
+    end
+
+    % Images size
+    image = imread(filename);
+    calibrationData.imageSize = size(image);
+else
+    % Edit a single measurement
+    image_index = varargin{1};
+    imagesPoints = calibrationData.imagesPoints;
+    filename = fullfile(imagesFolder, baseFilename + "_" + string(image_index) + "." + ext);
+    imagesPoints(image_index, :) = mark_point(filename, 0.025);
 end
 
 % Calibration points images coordinates
-calibrationData.imagePoints = imagePoints;
-
-% Images size
-image = imread(filename);
-calibrationData.imageSize = size(image);
+calibrationData.imagesPoints = imagesPoints;
 
 save(calibrationData.file, "calibrationData");
 
