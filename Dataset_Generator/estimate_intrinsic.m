bc13f1f2208d4c225fc57a8107717f1e8367932e
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-09-20 17:16:02 +0200

    Create estimate_intrinsic.m

---
+++ Dataset_Generator/estimate_intrinsic.m
@@ -0,0 +1,25 @@
+function calibrationData = estimate_intrinsic(currentCalibrationData)
+
+calibrationData = currentCalibrationData;
+
+imagesPoints = calibrationData.imagesPoints;
+cameraPoints = calibrationData.cameraPoints;
+
+[intrinsicMatrix0, aspectRatio, skew, mre0, lmx, lmy] = linreg_estimate(imagesPoints, cameraPoints);
+[intrinsicMatrix, residuals, mre] = least_squares_estimate(imagesPoints, cameraPoints, aspectRatio, skew);
+
+
+calibrationData.intrinsicMatrix = intrinsicMatrix;
+calibrationData.residuals = residuals;
+calibrationData.meanReprojectionError = mre;
+
+imageSize = calibrationData.imageSize;
+fx = intrinsicMatrix(1, 1);
+fy = intrinsicMatrix(2, 2);
+[fov_hor, fov_ver] = fov(fx, fy, imageSize(2), imageSize(1));
+calibrationData.fov = [fov_hor, fov_ver];
+
+save(calibrationData.file, "calibrationData");
+
+end
+
