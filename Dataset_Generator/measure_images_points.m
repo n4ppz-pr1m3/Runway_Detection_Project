748856c9e8832f7ef498fb946caa472b026c3bc5
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-09-20 13:50:37 +0200

    Create measure_images_points.m

---
+++ Dataset_Generator/measure_images_points.m
@@ -0,0 +1,31 @@
+function calibrationData = measure_images_points(currentCalibrationData)
+
+calibrationData = currentCalibrationData;
+
+imagesFolder = calibrationData.imagesFolder;
+baseFilename = calibrationData.baseFilename;
+ext = calibrationData.imageFormat;
+numImages = calibrationData.numImages;
+
+imagePoints = zeros(numImages, 2);
+for i=1:numImages
+    filename = imagesFolder + "/" + baseFilename + "_" + string(i) + "." + ext;
+    %imagePoints(i, :) = mark_image(filename, 1, 1, [], '');
+    imagePoints(i, :) = mark_point(filename, 0.025);
+end
+
+% Calibration points images coordinates
+calibrationData.imagePoints = imagePoints;
+
+% Images size
+I = imread(filename);
+calibrationData.imageSize = size(I);
+
+save(calibrationData.file, "calibrationData");
+
+
+
+
+
+end
+
