c3d90d8b134e568079364611fc518bba09be7d79
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-09-20 22:41:04 +0200

    Update intrinsic_calibration.m

--- Dataset_Generator/intrinsic_calibration.m
+++ Dataset_Generator/intrinsic_calibration.m
@@ -1,24 +1,32 @@
-function calibrations = intrinsic_calibration(calibrations_parameters)
+function calibrationData = intrinsic_calibration(numImages, dpi, calibrationFolder,...
+                                baseFilename, imageFormat)
 
-nCalibrations = numel(calibrations_parameters);
-calibrations = cell(nCalibrations, 1);
-for i=1:nCalibrations
-    calibration_parameters = calibrations_parameters{i};
-    numImages = calibration_parameters.numImages;
-    dpi = calibration_parameters.dpi;
-    calibrationFolder = calibration_parameters.calibrationFolder;
-    baseFilename = calibration_parameters.baseFilename;
-    imageFormat = calibration_parameters.imageFormat;
+calibrationData = setup_calibration(numImages, dpi, calibrationFolder,...
+                                baseFilename, imageFormat);
+calibrationData = generate_pdf_calibration_images(calibrationData);
+calibrationData = convert_pdf_calibration_images(calibrationData);
+calibrationData = measure_images_points(calibrationData);
+calibrationData = estimate_intrinsic(calibrationData);
+pause(0.1);
 
-    calibrationData = setup_calibration(numImages, dpi, calibrationFolder,...
-                                    baseFilename, imageFormat);
-    calibrationData = generate_pdf_calibration_images(calibrationData);
-    calibrationData = convert_pdf_calibration_images(calibrationData);
-    calibrationData = measure_images_points(calibrationData);
-    calibrationData = estimate_intrinsic(calibrationData);
-    show_reprojections(calibrationData)
+% Fine tuning
+while true
+    prompt = "Enter an integer in the range 1-" + string(numImages) + " to edit the corresponding image or press Return to quit: ";
+    image_index = input(prompt);
+    if isempty(image_index)
+        image_index = 0;
+    end
 
-    calibrations{i} = calibrationData;
+    if (image_index >= 1) && (image_index <= numImages)
+        close all;
+        calibrationData = measure_images_points(calibrationData, image_index);
+        calibrationData = estimate_intrinsic(calibrationData);
+        pause(0.1);
+    else
+        break;
+    end
 end
+show_reprojections(calibrationData)
+
 end
 
