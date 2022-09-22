5035d3ce3fe87d418e847b15563ada1c5a22b16e
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-09-22 02:31:41 +0200

    Update convert_pdf_calibration_images.m

--- Dataset_Generator/convert_pdf_calibration_images.m
+++ Dataset_Generator/convert_pdf_calibration_images.m
@@ -12,7 +12,8 @@ disp(newline + "Converting pdf calibration images to " + imageFormat)
 
 % Estimates crop
 test_file = fullfile(sourceFolder, baseFilename + "_1.pdf");
-cmd = "convert -density " + dpi + " -depth 8 -quality 100 " + test_file + " tmp.png";
+tmp_img = "tmp." + imageFormat;
+cmd = "convert -density " + dpi + " -depth 8 -quality 100 " + test_file + " " + tmp_img;
 
 % Linux
 if isunix
@@ -24,7 +25,7 @@ end
 if status
     disp('Conversion error')
 else
-    image = imread("tmp.png");
+    image = imread(tmp_img);
     cropValue = string(ceil(1e-2 * size(image, 1)));
     calibrationData.cropValue = cropValue;
     save(calibrationData.file, "calibrationData");
@@ -34,7 +35,7 @@ end
 % Images conversion
 disp("Converting images...")
 cmd = "mogrify -format " + imageFormat + " -path " + destinationFolder + ...
-    " -density " + string(dpi) + " -depth 8 -quality 100 -gravity South -chop 0x" +...
+    " -density " + dpi + " -depth 8 -quality 100 -gravity South -chop 0x" +...
     cropValue + " " + sourceFolder + "/*.pdf";
 
 % Linux
