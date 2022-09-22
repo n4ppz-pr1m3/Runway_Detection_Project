f9de9f08e0bda17339c1361913e9610cacae6263
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-09-22 21:57:59 +0200

    Update generate_homogenous_predataset.m

--- Dataset_Generator/generate_homogenous_predataset.m
+++ Dataset_Generator/generate_homogenous_predataset.m
@@ -1,4 +1,4 @@
-function [dataset_size, labels_data] = generate_homogenous_predataset(airports_data,...
+function [labels_data, poses_data, dataset_size] = generate_homogenous_predataset(airports_data,...
                                                     coord_type, param1, param2, param3,...
                                                     calibrationData,...
                                                     preDatasetFolder, pdfImagesFolder,...
@@ -9,7 +9,7 @@ if isempty(preDatasetFolder)
 end
    
 if isempty(pdfImagesFolder)
-    pdfImagesFolder = preDatasetFolder + "/PDF_Images";
+    pdfImagesFolder = fullfile(preDatasetFolder, "PDF_Images");
 end
 
 if check_dir && ~mkdir(".", preDatasetFolder)
@@ -19,14 +19,14 @@ end
 % Generates poses
 disp("Generating poses...")
 [poses_data, dataset_size] = generate_poses(airports_data, coord_type, param1, param2, param3, offset);
-range = string(offset+1) + "_" + string(offset+dataset_size);
-save(fullfile(preDatasetFolder, "poses_data_" + range +".mat"), "poses_data"); % Debug
+% range = string(offset+1) + "_" + string(offset+dataset_size);
+% save(fullfile(preDatasetFolder, "poses_data_" + range +".mat"), "poses_data"); % Debug
 disp("... done")
 
 % Generates labels
 disp("Generating labels...")
 labels_data = generate_labels(airports_data, poses_data, calibrationData, basename);
-save(fullfile(preDatasetFolder, "labels_data_" + range + ".mat"), "labels_data"); % Debug
+%save(fullfile(preDatasetFolder, "labels_data_" + range + ".mat"), "labels_data"); % Debug
 disp("... done")
 
 % Generates pdf images
