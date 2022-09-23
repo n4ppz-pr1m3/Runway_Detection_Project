5818553a45499c582e18eb2426b43cbd1e4d25d4
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-09-23 17:05:57 +0200

    Update generate_images.m

--- Dataset_Generator/generate_images.m
+++ Dataset_Generator/generate_images.m
@@ -1,16 +1,19 @@
-function generate_images(pdfImagesFolder, imagesFolder, calibration_data, check_dir)
+function generate_images(calibration_data, pdfImagesFolder, tmpFolder, imagesFolder, basename, check_dir)
 
 if check_dir && ~mkdir(".", imagesFolder)
     error("Unable to create " + imagesFolder);
 end
 
+
+% Images conversion
+disp(newline + "Converting images...")
 imageFormat = calibration_data.imageFormat;
 dpi = calibration_data.dpi;
 cropValue = calibration_data.cropValue;
 
-cmd = "mogrify -format " + imageFormat + " -path " + imagesFolder + ...
-    " -density " + string(dpi) + " -depth 8 -quality 100 -gravity South -chop 0x" +...
-    string(cropValue) + " " + pdfImagesFolder + "/*.pdf";
+cmd = "mogrify -format " + imageFormat + " -path " + tmpFolder + ...
+    " -density " + dpi + " -depth 8 -quality 100 -gravity South -chop 0x" +...
+    cropValue + " " + pdfImagesFolder + "/*.pdf";
 
 % Linux
 if isunix
@@ -23,7 +26,24 @@ end
 
 if status
     disp('Conversion error')
+else
+    disp("... done")
 end
+
+% Images renaming
+tic
+disp("Renaming images...")
+[~, images_names, ext] = get_names(tmpFolder);
+for i=1:numel(images_names)
+    name = fullfile(tmpFolder, strcat(images_names{i}, ext{i}));
+    new_name = fullfile(imagesFolder, strcat(basename, images_names{i}, ext{i}));
+    [status, msg] = movefile(name, new_name);
+    if status
+        disp(msg)
+    end
+end
+disp("... done")
+toc
 
 end
 
