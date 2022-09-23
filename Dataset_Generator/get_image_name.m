9db8fbbfe7ee0803f22063f0bdb14390e47e3cce
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-09-23 17:05:50 +0200

    Update get_image_name.m

--- Dataset_Generator/get_image_name.m
+++ Dataset_Generator/get_image_name.m
@@ -1,4 +1,4 @@
-function image_name = get_image_name(basename, image_index)
-image_name  = basename + pad(string(image_index), 8, "left", "0");
+function image_name = get_image_name(basename, image_index, pad_value)
+image_name  = strcat(basename, pad(int2str(image_index), pad_value, "left", '0'));
 end
 
