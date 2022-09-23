0fa666ae9c9ac2c5b3a3dddaa9e2fa65597cfcc6
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-09-23 23:36:04 +0200

    Update get_image_name.m

--- Dataset_Generator/get_image_name.m
+++ Dataset_Generator/get_image_name.m
@@ -2,3 +2,8 @@ function image_name = get_image_name(basename, image_index, pad_value)
 image_name  = strcat(basename, pad(int2str(image_index), pad_value, "left", '0'));
 end
 
+
+
+% function name = format_indexed_name(basename, index, pad_value)
+% name  = strcat(basename, pad(int2str(index), pad_value, "left", '0'));
+% end
