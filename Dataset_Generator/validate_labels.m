5be87e37c6eb65cf0d107658d1872df478cacc99
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-09-23 17:05:38 +0200

    Update validate_labels.m

--- Dataset_Generator/validate_labels.m
+++ Dataset_Generator/validate_labels.m
@@ -15,31 +15,35 @@ if validation_ratio
     fig = figure;
     fig.Visible = "off";
     colors = linspecer(10, 'sequential');
+    [~, images_names, extensions] = get_names(imagesFolder);
     for i=1:numel(full_labels_data)
-        imageFormat = full_calibration_data{i}.imageFormat;
         labels_data = full_labels_data{i};
-        images_names = string(fieldnames(labels_data));
-        nImages = numel(images_names);
+        labels_names = fieldnames(labels_data);
+
+        nImages = numel(labels_names);
         sample_size = min([nImages, max([1, round(validation_ratio*nImages)])]);
         sample_indices = randperm(nImages, sample_size);
         for j=sample_indices
-            image_name = images_names(j);
-            image_path = fullfile(imagesFolder, image_name  + "." + imageFormat);
-            airport = labels_data.(image_name).airport;
+            label_name = labels_names{j};
+            image_index = str2double(label_name(4:end));
+            image_name = images_names{image_index};
+            ext = extensions{image_index};
+            image_path = fullfile(imagesFolder, strcat(image_name, ext));
+            airport = labels_data.(label_name).airport;
     
             % Draw borders
-            save_path = fullfile(validationFolder, "val_img_borders_" + string(index) + "." + imageFormat);
-            runways_corners = labels_data.(image_name).corners;
+            save_path = fullfile(validationFolder, "val_img_borders_" + string(index) + ext);
+            runways_corners = labels_data.(label_name).corners;
             draw_borders(airport, image_path, save_path, fig, colors, runways_corners)
     
             % Draw bounding boxes
-            runways_bounding_boxes = labels_data.(image_name).boxes;
-            save_path = fullfile(validationFolder, "val_img_bbox_" + string(index) + "." + imageFormat);
+            runways_bounding_boxes = labels_data.(label_name).boxes;
+            save_path = fullfile(validationFolder, "val_img_bbox_" + string(index) + ext);
             draw_bounding_boxes(airport, image_path, save_path, fig, colors, runways_bounding_boxes)
     
             % Draw segmentation masks
-            runways_segmentation_masks = labels_data.(image_name).masks;
-            save_path = fullfile(validationFolder, "val_img_seg_masks_" + string(index) + "." + imageFormat);
+            runways_segmentation_masks = labels_data.(label_name).masks;
+            save_path = fullfile(validationFolder, "val_img_seg_masks_" + string(index) + ext);
             draw_segmentation_masks(airport, image_path, save_path, fig, colors, runways_segmentation_masks)
     
             index = index + 1;
