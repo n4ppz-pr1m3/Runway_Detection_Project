61f082ab80ae971668917fcdb5227593192066ea
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-09-27 21:22:04 +0200

    Update update_heterogenous_predataset_labels.m

--- Dataset_Generator/update_heterogenous_predataset_labels.m
+++ Dataset_Generator/update_heterogenous_predataset_labels.m
@@ -1,21 +1,21 @@
-function new_full_labels_data = update_heterogenous_predataset_labels(full_airports_data,...
+function full_labels_data = update_heterogenous_predataset_labels(full_airports_data,...
                                                                     new_full_calibrationData,...
                                                                     preDatasetFolder,...
                                                                     pad_value)
-
-full_poses_path = fullfile(preDatasetFolder, "full_poses_data.mat");                         
-load(full_poses_path, "full_poses_data");
+                       
+load(fullfile(preDatasetFolder, "full_poses_data.mat"), "full_poses_data");
 nSubDatasets = numel(full_airports_data);
-new_full_labels_data = cell(nSubDatasets, 1);
+full_labels_data = cell(nSubDatasets, 1);
 for i=1:nSubDatasets
     disp("Updating labels for sub-dataset " + string(i) + " ...");
     airports_data = full_airports_data{i};
     poses_data = full_poses_data{i};
     calibrationData = new_full_calibrationData{i};
-    new_full_labels_data{i} = generate_labels(airports_data, poses_data, calibrationData, pad_value);                                                     
+    full_labels_data{i} = generate_labels(airports_data, poses_data, calibrationData, pad_value);                                                     
     disp("... done")
 end
-save(full_poses_path, "full_poses_data");
+% Overwrite existing labels data
+save(fullfile(preDatasetFolder, "full_labels_data.mat"), "full_labels_data");
 
 end
 
