01bc0ee572992bea04473de04b8c54e0748d95c7
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-09-27 21:21:49 +0200

    Create save_labels.m

---
+++ Dataset_Generator/save_labels.m
@@ -0,0 +1,13 @@
+function save_labels(preDatasetFolder, datasetName)
+
+disp("Saving labels...")
+labels_file = fullfile(preDatasetFolder, "full_labels_data.mat");
+named_labels_file = fullfile(preDatasetFolder, strcat("full_labels_data_", datasetName, ".mat"));
+[status, msg] = copyfile(labels_file, named_labels_file);
+if status
+    disp(msg);
+end
+disp("Labels saved at : " + named_labels_file);
+
+end
+
