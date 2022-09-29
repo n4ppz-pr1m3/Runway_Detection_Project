b71c7b5ccece3e7f18600b25bc30d1b791ded2df
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-09-29 21:26:40 +0200

    Update angleLessThan180.m

--- Dataset_Generator/angleLessThan180.m
+++ Dataset_Generator/angleLessThan180.m
@@ -1,7 +1,20 @@
-function bool = angleLessThan180(v1, v2, orientation)
+% function bool = angleLessThan180(v1, v2, orientation)
 
 % Check if the angle between 2 vectors measured according to the specified
 % orientation is less than 180 degrees.
+
+% angleLessThan180(v1, v2, "direct") returns true if 0 <= (v1, v2) <= 180.
+% angleLessThan180(v1, v2, "indirect") returns true if 0 <= (v2, v1) <= 180.
+
+% Input :
+% v1 (2 1-d double array) : first 2-vector
+% v2 (2 1-d double array) : second 2-vector
+% orientation (string) : specify how the angle between v1 and v2 is measured ("direct"|"indirect")
+
+% Output :
+% bool (boolean) : test result
+
+function bool = angleLessThan180(v1, v2, orientation)
 
 if ~isVector2(v1) || ~isVector2(v2)
     error("v1 and v2 must length 2 vectors")
