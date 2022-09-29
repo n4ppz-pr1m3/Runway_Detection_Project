f71608eb9783fae4df53928547037fa25a8dc6a3
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-09-29 21:26:49 +0200

    Update convex_polygon_area.m

--- Dataset_Generator/convex_polygon_area.m
+++ Dataset_Generator/convex_polygon_area.m
@@ -1,3 +1,13 @@
+% function area = convex_polygon_area(polygon)
+
+% Computes the area of a convex polygon
+
+% Input :
+% polygon (2xN 2-d double array) : polygon vertices cartesian coordinates
+
+% Output :
+% area (double) : polygon area
+
 function area = convex_polygon_area(polygon)
 
 assert(is_convex(polygon), "polygon must be convex")
