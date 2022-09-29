9590724b1f32dcb52cf1d5027e826b4da3e336df
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-09-29 21:26:43 +0200

    Update composite_mask.m

--- Dataset_Generator/composite_mask.m
+++ Dataset_Generator/composite_mask.m
@@ -1,3 +1,26 @@
+% function mask = composite_mask(masks)
+
+% Computes a matrix with the same filling pattern as a logical OR of a set
+% of masks (logical 2-d matrix) such that the non zeros locations of the ith
+% mask are set to i in the new matrix if they were null in all the previous masks.
+
+% Example :
+% Input masks:
+% [1, 0, 0;  , [0, 0, 1; , [0, 0, 0;
+%  0, 1, 0;  ,  0, 1, 0; ,  0, 0, 0;
+%  0, 0, 1]  ,  1, 0, 0] ,  1, 1, 1]
+
+% Output composite mask:
+% [1, 0, 2;
+%  0, 1, 0;
+%  2, 3, 1]
+
+% Input :
+% masks (MxNxP 3-d logical array) : P input masks
+
+% Output :
+% mask (MxN 2-d integer array) : output mask
+
 function mask = composite_mask(masks)
 mask = zeros(size(masks, 1:2), "uint8");
 filled_indices = [];
