0032cee47006c48ca43c679a474ff490005d0b4c
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-10-01 20:26:07 +0200

    Update draw_bounding_boxes.m

--- Dataset_Generator/draw_bounding_boxes.m
+++ Dataset_Generator/draw_bounding_boxes.m
@@ -1,3 +1,18 @@
+% function draw_bounding_boxes(airport, image_path, save_path, fig, colors, runways_bounding_boxes)
+
+% Draws the runways bounding boxes on an airport image.
+
+% The bounding boxes are specified by their top-left/bottom-right corners
+% in the format [xMin, yMin, xMax, yMax].
+
+% Input :
+% airport (string) : airport name
+% image_path (string) : path to the image file
+% save_path (string) : save path of the edited image
+% fig (figure handle) : figure object used to display the image
+% colors (Nx3 2-d double array) : set of RGB colors
+% runways_bounding_boxes (Nx4 2-d double array) : bounding boxes of the airport's runways
+
 function draw_bounding_boxes(airport, image_path, save_path, fig, colors, runways_bounding_boxes)
 
 imshow(image_path);
