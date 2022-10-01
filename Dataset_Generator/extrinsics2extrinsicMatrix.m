93e1387e72ed503324523acde388eecd178248e5
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-10-01 20:26:34 +0200

    Update extrinsics2extrinsicMatrix.m

--- Dataset_Generator/extrinsics2extrinsicMatrix.m
+++ Dataset_Generator/extrinsics2extrinsicMatrix.m
@@ -1,3 +1,17 @@
+% function extrinsicMatrix = extrinsics2extrinsicMatrix(extrinsics)
+
+% Converts an extrinsics vector to a matrix form.
+
+% Given an extrinsics vector vE = [vR; T] with rotation vector vR and
+% translation vector T, the associated extrinsic matrix is E = [R; T] with
+% R being the rotation matrix computed from vR with Rodrigues' formula.
+
+% Input :
+% extrinsics (6 1-d double array) : extrinsics vector
+
+% Output :
+% extrinsicMatrix (3x4 2-d double array) : extrinsic matrix
+
 function extrinsicMatrix = extrinsics2extrinsicMatrix(extrinsics)
 rotationVector = extrinsics(1:3);
 translationVector = extrinsics(4:6);
