3cba7dd8cbf355745f0079d5aae6c3343ba3ae0a
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-10-01 20:26:37 +0200

    Update format_indexed_name.m

--- Dataset_Generator/format_indexed_name.m
+++ Dataset_Generator/format_indexed_name.m
@@ -1,3 +1,18 @@
+% function name = format_indexed_name(basename, index, pad_value)
+
+% Composes a string and a number to form an indexed string with set padding
+% value.
+
+% Example : format_indexed_name("image", 5, 3) = "image005".
+
+% Input :
+% basename (string) : base string
+% index (integer) : string index
+% pad_value (integer) : padding value
+
+% Output :
+% name (string) : composed string
+
 function name = format_indexed_name(basename, index, pad_value)
 name  = strcat(basename, pad(int2str(index), pad_value, "left", '0'));
 end
