ce0212572a16a7b7628ae5cc24553d50deba3d8d
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-10-02 19:59:51 +0200

    Update read_json.m

--- Dataset_Generator/read_json.m
+++ Dataset_Generator/read_json.m
@@ -1,3 +1,14 @@
+
+% function data = read_json(filename)
+
+% Converts json data to matlab struct data.
+
+% Input :
+% filename (string) : path to json file
+
+% Output :
+% data (struct) : converted json data
+
 function data = read_json(filename)
 fileId = fopen(filename); 
 raw_data = fread(fileId, inf); 
