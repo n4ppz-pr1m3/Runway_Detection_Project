b75fe52cd11b86b50ab261da6e56a3b51451ede7
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-10-02 19:59:38 +0200

    Update get_bbox.m

--- Dataset_Generator/get_bbox.m
+++ Dataset_Generator/get_bbox.m
@@ -1,6 +1,21 @@
-function [bbox_plot, bbox_corners, bbox_center_size] = get_bbox(points)
+% function [bbox_plot, bbox_corners, bbox_center_size] = get_bbox(points)
 
 % Compute the bounding box coordinates of a set of 2d points
+
+% Three output are provided:
+% - bbox_plot : images coordinates of the bounding box corners
+% - bbox_corners : 4-vector representation in the form [xMin, yMin, xMax, yMax] (top-left/bottom-right corners)
+% - bbox_center_size : 4-vector representation in the form [xCenter, yCenter, bboxWidth, bboxHeight] (center/size)
+
+% Input :
+% points (2xN 2-d double array) : input points image coordinates
+
+% Output :
+% bbox_plot (2x4 2-d double array) : bounding box corners
+% bbox_corners (4 1-d double array) : bounding box top-left/bottom-right corners vector
+% bbox_center_size (4 1-d double array) : bounding box center/size vector
+
+function [bbox_plot, bbox_corners, bbox_center_size] = get_bbox(points)
 
 if (numel(size(points)) ~= 2) || (size(points, 1) ~= 2)
     error("points is expected to have size 2xN")
