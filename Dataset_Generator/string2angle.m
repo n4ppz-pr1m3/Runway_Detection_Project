3d059bc48c98c737fcb0d668e6938ca231e8a18c
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-10-02 20:00:14 +0200

    Update string2angle.m

--- Dataset_Generator/string2angle.m
+++ Dataset_Generator/string2angle.m
@@ -1,3 +1,13 @@
+
+% function angle = string2angle(str)
+
+% Computes an angle from a string representation.
+
+% The input string should have the form 'cardinal_direction + d.m.s.' angle
+
+% Example :
+% angle = string2angle("N12-34-56.78")
+
 function angle = string2angle(str)
 new_str = convertStringsToChars(str);
 angle = str2angle([new_str(2:end), new_str(1)]);
