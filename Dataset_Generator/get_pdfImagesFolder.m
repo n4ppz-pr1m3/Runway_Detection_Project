b857ec2bb203086c91d9774d3cdd80e174e096c1
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-10-02 20:00:33 +0200

    Update get_pdfImagesFolder.m

--- Dataset_Generator/get_pdfImagesFolder.m
+++ Dataset_Generator/get_pdfImagesFolder.m
@@ -1,3 +1,13 @@
+% function pdfImagesFolder = get_pdfImagesFolder(preDatasetFolder)
+
+% Returns the path to the pdf images folder of a pre-dataset.
+
+% Input :
+% preDatasetFolder (string) : pre-dataset folder path
+
+% Output :
+% pdfImagesFolder (string) : pdf images folder path
+
 function pdfImagesFolder = get_pdfImagesFolder(preDatasetFolder)
 pdfImagesFolder = fullfile(preDatasetFolder, "PDF_Images");
 end
