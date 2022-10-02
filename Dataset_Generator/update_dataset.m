fce38a1cbd55201ede103f43cc4bd0e8e4015d3f
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-10-02 20:00:49 +0200

    Update update_dataset.m

--- Dataset_Generator/update_dataset.m
+++ Dataset_Generator/update_dataset.m
@@ -1,3 +1,36 @@
+
+% function update_dataset(full_airports_data,...
+%                         updated_full_calibration_data,...
+%                         preDatasetFolder,...
+%                         basename, pad_value,...
+%                         newDatasetName, newDatasetFolder, newImagesFolder, newAnnotationsFolder, newMasksFolder,...
+%                         newValidationFolder, validation_ratio)
+
+% Creates a new dataset from an existing one.
+
+% The only difference allowed between the new dataset and the existing one
+% are the calibration settings. Airports used and camera views remain
+% identical. This allows the creation of an identical dataset with
+% different images sizes/formats.
+
+% The pdf source images being reused, their generation is skipped making
+% the dataset creation faster than building it from scratch. Time is mostly
+% spent on the images conversion to the new target sizes and formats.
+
+% Input :
+% full_airports_data (N 1-d cell array of airports data struct) : airports data associated with each sub-dataset
+% updated_full_calibration_data (N 1-d cell array of calibration data struct) : new calibration data associated with each sub-dataset
+% preDatasetFolder (string) : path to the pre-dataset folder
+% basename (string) : base dataset filenames
+% pad_value (integer) : padding value for files indexing
+% newDatasetName (string) : new dataset name
+% newDatasetFolder (string) : new dataset folder path
+% newImagesFolder (string) :  new dataset images folder name
+% newAnnotationsFolder (string) : new dataset annotations folder name
+% newMasksFolder (string) : new dataset masks folder name
+% newValidationFolder (string) : new validation folder path
+% validation_ratio (double) : sampling rate used for validation
+
 function update_dataset(full_airports_data,...
                         updated_full_calibration_data,...
                         preDatasetFolder,...
