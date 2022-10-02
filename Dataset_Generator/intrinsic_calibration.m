6cf6ad402162b0cf0d280bc26161f46853aa2e0d
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-10-02 19:59:23 +0200

    Update intrinsic_calibration.m

--- Dataset_Generator/intrinsic_calibration.m
+++ Dataset_Generator/intrinsic_calibration.m
@@ -1,3 +1,43 @@
+% function calibrationData = intrinsic_calibration(numImages, dpi, calibrationFolder,...
+%                                 baseFilename, imageFormat, default_calibration)
+
+% Interactive application to perform camera calibration.
+
+% Calibration procedure
+% Estimates the intrinsic parameters of a camera by using multiple views of
+% a single control point with known camera and image coordinates.
+
+% Pipeline
+% 0 - Calibration parameters initialization
+% 1a - Calibration images generation in pdf format
+% 1b - Camera coordinates computation
+% 2 - Calibration images conversion in target size and format
+% 3 - Images coordinates computation
+% 4 - [Optional] Re-computation of selected image coordinates
+% 5 - Intrinsic parameters estimation
+
+% Step 3 being a manual process, multiple iterations of step 4 followed by
+% step 5 might be necessary for accurate estimations.
+
+% The only way to specify the target image size is via the dpi (dot per inch).
+% For a "natural" rendering, a value of 96 is recommended. Higher values
+% will lead to higher resolutions (but at the cost of image quality) and
+% vice versa.
+
+% If default_calibration is set to true, in addition to being saved in
+% calibrationFolder, calibrationData will be saved in the local workspace.
+
+% Input :
+% numImages (integer) : number of images to perform calibration
+% dpi (integer) : dpi used for converting source pdf images
+% calibrationFolder (string) : calibration folder path
+% baseFilename (string) : calibration images basename
+% imageFormat (string) : target image format
+% default_calibration (boolean) : set the calibration settings as default
+
+% Output :
+% calibrationData (calibration data struct) : calibration settings
+
 function calibrationData = intrinsic_calibration(numImages, dpi, calibrationFolder,...
                                 baseFilename, imageFormat, default_calibration)
 
@@ -32,6 +72,7 @@ while true
         break;
     end
 end
+% Visualize reprojections
 show_reprojections(calibrationData)
 
 disp(newline + "Calibration complete")
