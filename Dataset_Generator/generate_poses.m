cda75de22f8d6f75f11f10e6bcce49e678b1b396
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-10-04 20:22:20 +0200

    Update generate_poses.m

--- Dataset_Generator/generate_poses.m
+++ Dataset_Generator/generate_poses.m
@@ -1,10 +1,10 @@
 
-% function [poses_data, poses_count] = generate_poses(airports_data, coord_type, param1, param2, param3, offset)
+% function [poses_data, poses_count] = generate_poses(runways_data, coord_type, param1, param2, param3, offset)
 
 % Generates poses data from camera coordinates.
 
 % Input :
-% airports_data (airports data struct) : airports data
+% runways_data (runways data struct) : runways data
 % coord_type (string) : coordinates system ('cylindrical'|'spherical')
 % param1 (1-d double array) : set of first components coordinates
 % param2 (1-d double array) : set of second components coordinates
@@ -15,17 +15,17 @@
 % poses_data (poses data struct) : poses data
 % poses_count (integer) : number of poses generated
 
-function [poses_data, poses_count] = generate_poses(airports_data, coord_type, param1, param2, param3, offset)
+function [poses_data, poses_count] = generate_poses(runways_data, coord_type, param1, param2, param3, offset)
 
-airports = string(fieldnames(airports_data));
+airports = string(fieldnames(runways_data));
 nAirports = numel(airports);
 poses_count = nAirports * numel(param1) * numel(param2) * numel(param3);
 poses_data = [];
 for i=1:nAirports
     % Camera poses
     airport = airports(i);
-    airport_data = airports_data.(airport);
-    airport_origin = airport_data{1};
+    airport_data = runways_data.(airport);
+    airport_origin = airport_data.origin;
     cameraPoses = coord2poses(coord_type, param1, param2, param3, airport_origin);
     poses_data.(airport).poses = cameraPoses;
 
