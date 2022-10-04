39f52877697c9dc38a2f4af0d01cb7fc8344cddf
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-10-04 19:18:17 +0200

    Update write_json.m

--- Dataset_Generator/write_json.m
+++ Dataset_Generator/write_json.m
@@ -1,3 +1,12 @@
+
+% function write_json(data, filename)
+
+% Converts matlab struct to json.
+
+% Input:
+% data (struct) : input struct
+% filename (string) : json file output path
+
 function write_json(data, filename)
 fileId = fopen(filename, 'w');
 json_data = jsonencode(data);
