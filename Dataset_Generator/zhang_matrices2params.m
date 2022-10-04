bb13900049469d88f24d72bea4e0d7e4bfeb37a1
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-10-04 19:18:19 +0200

    Update zhang_matrices2params.m

--- Dataset_Generator/zhang_matrices2params.m
+++ Dataset_Generator/zhang_matrices2params.m
@@ -1,3 +1,16 @@
+
+% function p = zhang_matrices2params(intrinsicMatrix, extrinsics)
+
+% Composes a Zhang calibration parameters vector from an intrinsic matrix
+% and a global extrinsics vector.
+
+% Input :
+% intrinsicMatrix (3x3 2-d double array) : intrinsic matrix
+% extrinsics (6xN 1-d double array) : stacked extrinsics of all views
+
+% Output :
+% p (3+6xN 1-d double array) : calibration parameters vector
+
 function p = zhang_matrices2params(intrinsicMatrix, extrinsics)
 
 f = intrinsicMatrix(1, 1);
