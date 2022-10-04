18db970d6022b197157316fa8a3313728e40a135
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-10-04 20:22:09 +0200

    Update update_heterogenous_predataset_labels.m

--- Dataset_Generator/update_heterogenous_predataset_labels.m
+++ Dataset_Generator/update_heterogenous_predataset_labels.m
@@ -1,5 +1,5 @@
 
-% function full_labels_data = update_heterogenous_predataset_labels(full_airports_data,...
+% function full_labels_data = update_heterogenous_predataset_labels(full_runways_data,...
 %                                                                     new_full_calibrationData,...
 %                                                                     preDatasetFolder,...
 %                                                                     pad_value)
@@ -13,7 +13,7 @@
 % The existing label data is overwritten.
 
 % Input :
-% full_airports_data (1-d cell array of airport data struct) : airports data
+% full_runways_data (1-d cell array of runways data struct) : runways data
 % new_full_calibrationData (1-d cell array of calibration data struct) : updated calibration settings
 % preDatasetFolder (string) : pre-dataset folder path
 % pad_value (integer) : padding value
@@ -21,20 +21,20 @@
 % Output :
 % full_labels_data (1-d cell array of labels data struct) : updated labels data
 
-function full_labels_data = update_heterogenous_predataset_labels(full_airports_data,...
+function full_labels_data = update_heterogenous_predataset_labels(full_runways_data,...
                                                                     new_full_calibrationData,...
                                                                     preDatasetFolder,...
                                                                     pad_value)
                        
 load(fullfile(preDatasetFolder, "full_poses_data.mat"), "full_poses_data");
-nSubDatasets = numel(full_airports_data);
+nSubDatasets = numel(full_runways_data);
 full_labels_data = cell(nSubDatasets, 1);
 for i=1:nSubDatasets
     disp("Updating labels for sub-dataset " + string(i) + " ...");
-    airports_data = full_airports_data{i};
+    runways_data = full_runways_data{i};
     poses_data = full_poses_data{i};
     calibrationData = new_full_calibrationData{i};
-    full_labels_data{i} = generate_labels(airports_data, poses_data, calibrationData, pad_value);                                                     
+    full_labels_data{i} = generate_labels(runways_data, poses_data, calibrationData, pad_value);                                                     
     disp("... done")
 end
 % Overwrite existing labels data
