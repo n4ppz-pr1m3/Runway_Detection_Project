e11f1a4360232790919349b91f84e3d47d2fc717
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-10-04 19:18:29 +0200

    Update zhang_params2matrices.m

--- Dataset_Generator/zhang_params2matrices.m
+++ Dataset_Generator/zhang_params2matrices.m
@@ -1,3 +1,24 @@
+
+% function [intrinsicMatrix, extrinsics] = zhang_params2matrices(p)
+
+% Extracts the intrinsic matrix and extrinsics from a Zhang calibration
+% parameters vector.
+
+% p should have length 3+6xN where N is the number of images used
+% for calibration since an ideal camera with square pixels and no
+% distorsion is assumed.
+
+% The ith column of extrinsics is e_i := [vR_i; T_i] is the transformation
+% associated with view i with vR_i being the rotation vector and T_i the
+% translation.
+
+% Input :
+% p (1-d double array) : parameters vectors
+
+% Output :
+% intrinsicMatrix (3x3 2-d double array) : intrinsic matrix
+% extrinsics (6xN 2-d double array) : extrinsics vectors
+
 function [intrinsicMatrix, extrinsics] = zhang_params2matrices(p)
 
 intrinsicMatrix = [p(1), 0,    p(2);...
