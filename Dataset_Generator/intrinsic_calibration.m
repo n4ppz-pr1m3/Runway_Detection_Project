ce072a2bd8a1b0533f88f4e64e27d335ae34e362
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-10-05 21:00:07 +0200

    Update intrinsic_calibration.m

--- Dataset_Generator/intrinsic_calibration.m
+++ Dataset_Generator/intrinsic_calibration.m
@@ -1,5 +1,5 @@
 
-% function calibrationData = intrinsic_calibration(numImages, dpi, calibrationFolder,...
+% function calibration_data = intrinsic_calibration(numImages, dpi, calibrationFolder,...
 %                                 baseFilename, imageFormat, default_calibration)
 
 % Interactive application to perform camera calibration.
@@ -26,7 +26,7 @@
 % vice versa.
 
 % If default_calibration is set to true, in addition to being saved in
-% calibrationFolder, calibrationData will be saved in the local workspace.
+% calibrationFolder, calibration_data will be saved in the local workspace.
 
 % Input :
 % numImages (integer) : number of images to perform calibration
@@ -37,21 +37,21 @@
 % default_calibration (boolean) : set the calibration settings as default
 
 % Output :
-% calibrationData (calibration data struct) : calibration settings
+% calibration_data (calibration data struct) : calibration settings
 
-function calibrationData = intrinsic_calibration(numImages, dpi, calibrationFolder,...
+function calibration_data = intrinsic_calibration(numImages, dpi, calibrationFolder,...
                                 baseFilename, imageFormat, default_calibration)
 
 if ~exist("default_calibration", "var")
     default_calibration = true;
 end
 
-calibrationData = setup_calibration(numImages, dpi, calibrationFolder,...
+calibration_data = setup_calibration(numImages, dpi, calibrationFolder,...
                                 baseFilename, imageFormat);
-calibrationData = generate_pdf_calibration_images(calibrationData);
-calibrationData = convert_pdf_calibration_images(calibrationData);
-calibrationData = measure_images_points(calibrationData);
-calibrationData = estimate_intrinsic(calibrationData);
+calibration_data = generate_pdf_calibration_images(calibration_data);
+calibration_data = convert_pdf_calibration_images(calibration_data);
+calibration_data = measure_images_points(calibration_data);
+calibration_data = estimate_intrinsic(calibration_data);
 pause(0.1);
 
 disp("Fine tuning estimation")
@@ -66,21 +66,21 @@ while true
 
     if (image_index >= 1) && (image_index <= numImages)
         close all;
-        calibrationData = measure_images_points(calibrationData, image_index);
-        calibrationData = estimate_intrinsic(calibrationData);
+        calibration_data = measure_images_points(calibration_data, image_index);
+        calibration_data = estimate_intrinsic(calibration_data);
         pause(0.1);
     else
         break;
     end
 end
 % Visualize reprojections
-show_reprojections(calibrationData)
+show_reprojections(calibration_data)
 
 disp(newline + "Calibration complete")
-disp("Calibration data saved to " + calibrationData.file)
+disp("Calibration data saved to " + calibration_data.file)
 
 if default_calibration
-    save("CalibrationData.mat", "calibrationData")
+    save("calibration_data.mat", "calibration_data")
     disp("Calibration set to default. Saving calibration data to local workspace")
 end
 
