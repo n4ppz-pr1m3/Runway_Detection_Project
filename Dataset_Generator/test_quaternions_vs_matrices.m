bf8520dc26f5899a668ad14bc01ff8209bf8f094
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-10-31 12:32:51 +0100

    Update test_quaternions_vs_matrices.m

--- Dataset_Generator/test_quaternions_vs_matrices.m
+++ Dataset_Generator/test_quaternions_vs_matrices.m
@@ -3,9 +3,9 @@
 % transformation on random data when using quaternions and rotation matrices.
 
 clear; clc;
-cameraPose = [1000 * randn(1, 3), 100 * randn(1, 3)]; % Random camera pose
-worldPose = [1000 * randn(1, 3), 100 * randn(1, 3)];  % Random world pose
-worldPoint = 1000 * randn(3, 1);                      % Random world point
+cameraPose = [1000 * randn(1, 3), 100 * randn(1, 3)];   % Random camera pose
+worldPose = [1000 * randn(1, 3), 100 * randn(1, 3)];    % Random world pose
+worldPoint = 1000 * randn(3, 1);                        % Random world point
 
 mQuat = z_extrinsic_matrix(cameraPose, worldPose, 'quat');
 mRot = z_extrinsic_matrix(cameraPose, worldPose, 'rotm');
