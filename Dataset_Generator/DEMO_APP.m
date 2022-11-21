bd19623e575549643cbe651861d097e0cc14e3f5
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-11-21 20:38:39 +0100

    Update DEMO_APP.m

--- Dataset_Generator/DEMO_APP.m
+++ Dataset_Generator/DEMO_APP.m
@@ -1,4 +1,3 @@
-%% Intrinsic estimation
 clc; clear; close all;
 
 load("calibration_data.mat")
@@ -6,14 +5,3 @@ load("calibration_data.mat")
 load("runways_data.mat");
 
 detect_runway(runways_data, calibration_data, 'manual')
-
-%% Zhang estimation
-clc; clear; close all;
-
-load("./Calibration/Zhang/calibrationZhang.mat")
-intrinsicMatrix = calibrationZhang.intrinsicMatrix;
-%intrinsicMatrix = calibrationZhang.intrinsicMatrixMatlab;
-
-load("runwayData.mat");
-
-detect_runway(runwayData, intrinsicMatrix, 'manual')
