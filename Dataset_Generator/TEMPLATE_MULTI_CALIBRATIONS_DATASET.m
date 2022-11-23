aecdb744a60fecb4afefa143d7067672d6a964b7
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-11-23 20:27:30 +0100

    Update TEMPLATE_MULTI_CALIBRATIONS_DATASET.m

--- Dataset_Generator/TEMPLATE_MULTI_CALIBRATIONS_DATASET.m
+++ Dataset_Generator/TEMPLATE_MULTI_CALIBRATIONS_DATASET.m
@@ -18,10 +18,10 @@ calibration_data_1 = intrinsic_calibration(numImages, dpi, calibrationFolder,...
 
 % ----------------------------------------- Second Camera calibration
 numImages = 5;
-dpi = 50;
+dpi = 60;
 calibrationFolder = "Multi_Calibration_2";
 baseFilename = "calibration_image";
-imageFormat = "png";
+imageFormat = "jpg";
 default_calibration = false;
 
 calibration_data_2 = intrinsic_calibration(numImages, dpi, calibrationFolder,...
@@ -30,15 +30,17 @@ calibration_data_2 = intrinsic_calibration(numImages, dpi, calibrationFolder,...
 
 % ----------------------------------------- Dataset parameters
 coord_type = "spherical";
-param1 = [0, 90];
+param1 = [0, 180];
 param2 = [30, 60];
 param3 = [10000, 15000];
 
-full_camera_locations = {coord_type, param1, param2, param3;...
-                        coord_type, param1+45, param2+20, param3;...
-                        coord_type, param1-45, param2, param3};
+full_camera_locations = {coord_type, param1,    param2,    param3;...      % Parameters sub-dataset 1
+                         coord_type, param1+90, param2+20, param3;...      %       -    sub-dataset 2
+                         coord_type, param1-45, param2,    param3};        %       -    sub-dataset 3 
 
-full_calibration_data = {calibration_data_1, calibration_data_2, calibration_data_2};
+full_calibration_data = {calibration_data_1,...                            % Calibration sub-dataset 1
+                         calibration_data_2,...                            %       -     sub-dataset 2
+                         calibration_data_2};                              %       -     sub-dataset 3
 
 preDatasetFolder = "Pre_Dataset_Multi";
 
@@ -51,15 +53,15 @@ masksFolder = "Masks";
 render_times = [5, 2.5, 1];
 basename = "apt_image_";
 pad_value = 4;
-validationFolder = "Validation_PASCAL_VOC_Multi";
+validationFolder = "Validation_Multi";
 validation_ratio = 0.1;
 
 full_airports_data = {runways_data, runways_data, runways_data};
 
 generate_dataset(full_airports_data,...
-                full_camera_locations,...
-                full_calibration_data,...
-                preDatasetFolder,...
-                render_times, basename, pad_value,...
-                datasetName, datasetFolder, imagesFolder, annotationsFolder, masksFolder,...
-                validationFolder, validation_ratio);
+                 full_camera_locations,...
+                 full_calibration_data,...
+                 preDatasetFolder,...
+                 render_times, basename, pad_value,...
+                 datasetName, datasetFolder, imagesFolder, annotationsFolder, masksFolder,...
+                 validationFolder, validation_ratio);
