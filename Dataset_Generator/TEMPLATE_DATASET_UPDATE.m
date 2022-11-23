afb0b8cebf20ec28b54771e45cea14ad2a8fcfaf
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-11-23 20:27:24 +0100

    Update TEMPLATE_DATASET_UPDATE.m

--- Dataset_Generator/TEMPLATE_DATASET_UPDATE.m
+++ Dataset_Generator/TEMPLATE_DATASET_UPDATE.m
@@ -7,7 +7,7 @@ runways_data = get_runways_data(raw_airports_data, filter_list);
 
 % ----------------------------------------- Low Res Camera calibration
 numImages = 5;
-dpi = 40;
+dpi = 30;
 calibrationFolder = "Calibration_Low";
 baseFilename = "calibration_image";
 imageFormat = "png";
@@ -18,7 +18,7 @@ calibration_data_low = intrinsic_calibration(numImages, dpi, calibrationFolder,.
 
 % ----------------------------------------- High Res Camera calibration
 numImages = 5;
-dpi = 94;
+dpi = 50;
 calibrationFolder = "Calibration_Low";
 baseFilename = "calibration_image";
 imageFormat = "png";
@@ -37,9 +37,9 @@ full_camera_locations = {coord_type, param1, param2, param3};
 
 full_calibration_data = {calibration_data_low};
 
-preDatasetFolder = "Pre_Dataset";
+preDatasetFolder = "Pre_Dataset_Low_High";
 
-datasetName = "Example_Dataset";
+datasetName = "Example_Dataset_Low";
 datasetFolder = "Dataset_Low";
 imagesFolder = "Images";
 annotationsFolder = "Annotations";
@@ -48,7 +48,7 @@ masksFolder = "Masks";
 render_times = [5, 2.5, 1];
 basename = "apt_image_";
 pad_value = 4;
-validationFolder = "Validation_Low_PASCAL_VOC";
+validationFolder = "Validation_Low";
 validation_ratio = 0.1;
 
 full_airports_data = {runways_data};
@@ -66,7 +66,7 @@ generate_dataset(full_airports_data,...
 
 updated_full_calibration_data = {calibration_data_high};
 
-newDatasetName = "Example_Dataset";
+newDatasetName = "Example_Dataset_High";
 newDatasetFolder = "Dataset_High";
 newImagesFolder = "NEW_Images";
 newAnnotationsFolder = "NEW_Annotations";
@@ -75,7 +75,7 @@ newMasksFolder = "NEW_Masks";
 
 basename = "APT_IMG";
 pad_value = 4;
-newValidationFolder = "Validation_High_PASCAL_VOC";
+newValidationFolder = "Validation_High";
 validation_ratio = 0.15;
 
 
