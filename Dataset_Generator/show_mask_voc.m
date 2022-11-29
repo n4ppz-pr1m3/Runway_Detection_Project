4577219a11bc419c17d36d34032b7da16f24c05c
nbl-pr1m3 <113786598+nbl-pr1m3@users.noreply.github.com>
2022-11-29 21:14:49 +0100

    Update show_mask_voc.m

--- Dataset_Generator/show_mask_voc.m
+++ Dataset_Generator/show_mask_voc.m
@@ -1,3 +1,11 @@
+
+% function show_mask_voc(mask_file)
+
+% Visualize masks in Pascal VOC format.
+
+% Input :
+% mask_file (string) : mask image filename
+
 function show_mask_voc(mask_file)
 
 img_mask = double(imread(mask_file));
@@ -8,22 +16,20 @@ title("Class mask")
 height = size(img_mask, 1);
 width = size(img_mask, 2);
 instance_masks = zeros(height, width, 3);
-n_colors = max(img_mask, [], 'all')-2;
+n_colors = max(img_mask, [], 'all');
 colors = linspecer(n_colors, 'sequential');
 for i=1:n_colors
     sub_mask = img_mask == i;
-    R = uint8(255 * colors(i, 1));
-    G = uint8(255 * colors(i, 2));
-    B = uint8(255 * colors(i, 3));
     redMask = zeros([height, width, 3], "logical"); redMask(:, :, 1) = sub_mask;
     greenMask = zeros([height, width, 3], "logical"); greenMask(:, :, 2) = sub_mask;
     blueMask = zeros([height, width, 3], "logical"); blueMask(:, :, 3) = sub_mask;
-    instance_masks(redMask) = R;
-    instance_masks(greenMask) = G;
-    instance_masks(blueMask) = B;
+    instance_masks(redMask) = colors(i, 1);
+    instance_masks(greenMask) = colors(i, 2);
+    instance_masks(blueMask) = colors(i, 3);
 end
 figure;
 imshow(instance_masks);
 title("Instance masks")
+
 end
 
